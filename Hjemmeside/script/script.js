74483333175266cf977c7ab7401d6b48c9e5577f
Camilla E. Petersen <61732390+CamillaEPetersen@users.noreply.github.com>
2021-05-18 11:54:26 +0200

    Flere rettelser til responsive topnav

--- Hjemmeside/script/script.js
+++ Hjemmeside/script/script.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* RESPONSIVE TOPNAV - Skift mellem normal og dropdown version*/
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
